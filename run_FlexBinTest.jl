41959c9de52ea18bd563dea9cea338c4c961539a
mgwolf <michael.wolf@nasa.gov>
2026-02-27 20:12:44 -0500

    Updated test file to call first mdo file

--- run_FlexBinTest.jl
+++ run_FlexBinTest.jl
@@ -4,21 +4,23 @@ using FlexBinFPCLoader, DataFrames, XLSX
 xlsFileName = "mdoDUFF.xlsx" #"mdoFormat_v0d3.xlsx"
 wkDir = raw"C:\Users\mgwolf\Documents\MATLAB\Postflight_FMV\juliaWolfTools\FlexBinFPC"
 # dufs = FlexBinFPC.load_DUFs(joinpath(wkDir,xlsFileName))
-dufs = FlexBinFPCLoader.load_DUFs(joinpath(wkDir,xlsFileName))
+dufs = load_DUFs(joinpath(wkDir,xlsFileName))
 
-# # Define MDO file path
-# mdoPath = raw"C:\Users\mgwolf\Documents\MATLAB\Postflight_FMV\data\F9"
+# Define MDO file path
+mdoPath = raw"C:\Users\mgwolf\Documents\MATLAB\Postflight_FMV\data\F9"
 
-# # Example single run MDO Files
-# mdoFiles = ["sxTM_FH-009_GNC_26950410_20231229.mdo",
-#             "sxTM_FH-008_GNC_26127627_20231013.mdo",
-#             "psyche_xSimNom_FMA_Oct-12_20231011.mdo",
-#             "H01_FH-008_GNC_26030492_20231006.mdo",
-#             "IMAP_xSimNom_PGAA-2_20250429.mdo",
-#             "IMAP_xSimMC_PGAA-2_20250429.mdo",
-#             "sxTM_F9-235_GNC_24728169_20231127.mdo", # PSN - IMAP Demo
-#             "PSN_xSimNom_FMA.mdo",   # IMAP Demo XSim nominal
-#             "sxTM_F9-148_GNC_18081878_20220415.mdo"]
+# Example single run MDO Files
+mdoFiles = ["sxTM_FH-009_GNC_26950410_20231229.mdo",
+            "sxTM_FH-008_GNC_26127627_20231013.mdo",
+            "psyche_xSimNom_FMA_Oct-12_20231011.mdo",
+            "H01_FH-008_GNC_26030492_20231006.mdo",
+            "IMAP_xSimNom_PGAA-2_20250429.mdo",
+            "IMAP_xSimMC_PGAA-2_20250429.mdo",
+            "sxTM_F9-235_GNC_24728169_20231127.mdo", # PSN - IMAP Demo
+            "PSN_xSimNom_FMA.mdo",   # IMAP Demo XSim nominal
+            "sxTM_F9-148_GNC_18081878_20220415.mdo"]
+
+mdo = load_FlexBinFPC(joinpath(mdoPath,mdoFiles[1]),joinpath(wkDir,xlsFileName))
 
 # mdoData = Vector()
 # for fn in mdoFiles
